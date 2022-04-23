a557af485b907e2674030132810fc4d96df15e4e
Kittera <denizine@gmail.com>
2022-04-23 03:54:30 -0700

    Add new classes

---
+++ src/com/kittera/crypto487/application/Crypto487Application.java
@@ -0,0 +1,19 @@
+package com.kittera.crypto487.application;
+
+import java.util.Scanner;
+
+public class Crypto487Application {
+   public static void main(String... args) {
+      //set up i/o objects
+      Scanner console = new Scanner(System.in);
+      
+      
+      //loop here
+      console.close();
+      System.exit(0);
+   }
+   
+   private static void loop(Scanner console) {
+   
+   }
+}
